79103219d304fbae84734bff60206a2bad2c5664
Hardik Jain <jainhardik120@gmail.com>
2023-12-01 13:08:43 +0530

    Intiial Commit

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,36 @@
 /* eslint-disable object-curly-spacing */
 const { onRequest } = require("firebase-functions/v2/https");
-const {setGlobalOptions} = require("firebase-functions/v2");
-setGlobalOptions({maxInstances: 10});
+const { setGlobalOptions } = require("firebase-functions/v2");
+setGlobalOptions({ maxInstances: 10 });
 const { initializeApp } = require("firebase-admin/app");
-const {getMessaging} = require("firebase-admin/messaging");
+const { getMessaging } = require("firebase-admin/messaging");
 
 initializeApp();
 
 exports.sendNotification = onRequest(async (req, res) => {
+  const { deviceToken, message, title, data } = req.body;
+  if (!deviceToken || !message || !title) {
+    return res.status(400).json(
+        { error: "Missing deviceToken or message or title" },
+    );
+  }
+  const payload = {
+    token: deviceToken,
+    notification: {
+      title: title,
+      body: message,
+    },
+  };
+  if (data !== undefined) {
+    payload.data = data;
+  }
   try {
-    const { deviceToken, message, title } = req.body;
-    if (!deviceToken || !message || !title) {
-      return res.status(400).json(
-          { error: "Missing deviceToken or message or title" },
-      );
-    }
-    const payload = {
-      token: deviceToken,
-      notification: {
-        title: title,
-        body: message,
-      },
-    };
-
-    await getMessaging().send(payload);
-
-    return res.status(200).json({ success: "Notification sent successfully" });
+    const messageId = await getMessaging().send(payload);
+    return res.status(200).json({ success: "Notification sent successfully",
+      messageId: messageId,
+    });
   } catch (error) {
-    console.error("Error sending notification:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    console.error("Error sending notification:", error.errorInfo.message);
+    return res.status(500).json(error);
   }
 });
